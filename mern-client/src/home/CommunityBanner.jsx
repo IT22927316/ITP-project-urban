a3c7c5a32952c6a4e50c69ee94901e59a217ed79
IT22035394 <IT22035394@my.sliit.lk>
2024-04-22 10:46:44 +0530

    Updated community banner

--- mern-client/src/home/CommunityBanner.jsx
+++ mern-client/src/home/CommunityBanner.jsx
@@ -7,8 +7,8 @@ const CommunityBanner = () => {
     <div className='mt-16 py-12 bg-teal-100 px-4 lg:px-24'>
         <div className='flex flex-col md:flex-row justify-between items-center gap-12'>
             <div className='md:w-1/2'>
-                <h2 className='text-4xl font-bold mb-6 leading-snug'>Welcome To Our Urban Harvest Communities</h2>
-                <p>Join our communities for a dynamic congregation of urban growers and 
+                <h2 className='text-5xl font-bold mb-6 leading-snug'>Join and connect with our <span className='text-green-700'>Green Communities</span></h2>
+                <p className='mb-10 text-lg md:w-5/6'>Join our communities for a dynamic congregation of urban growers and 
                     eco-minded individuals committed to nurturing not just plants, but 
                     relationships and knowledge as well. In this collaborative haven, members
                      from all walks of life come together to exchange wisdom, share successes, 
@@ -17,6 +17,23 @@ const CommunityBanner = () => {
                       Engage in community-driven projects, participate in local meet-ups, and contribute 
                       to our resource pool. Whether you’re a novice plant parent or a seasoned soil steward, our
                        community is your fertile ground for growth, innovation, and connection.</p>
+
+                       {/* stats */}
+                <div className='flex flex-col sm:flex-row justify-between gap-6 md:w-3/4 my-14'>
+                    <div>
+                        <h3 className='text-3xl font-bold'>250+</h3>
+                        <p className='text-base'>Communities</p>
+                    </div>
+                    <div>
+                        <h3 className='text-3xl font-bold'>10+</h3>
+                        <p className='text-base'>Weekly Events</p>
+                    </div>
+                    <div>
+                        <h3 className='text-3xl font-bold'>1500+</h3>
+                        <p className='text-base'>Active Members</p>
+                    </div>
+                </div>
+
                 <Link to="/communitymain" className='mt-5 block'><button className='bg-green-700 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300'>Explore Communities</button></Link>
             </div>
             <div>
