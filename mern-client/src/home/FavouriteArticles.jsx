9338b44b4d7340e363ade80f922a5053f92714fc
IT22927316 <IT22927316@my.sliit.lk>
2024-05-06 21:13:48 +0530

    Update FavouriteArticles.jsx

--- mern-client/src/home/FavouriteArticles.jsx
+++ mern-client/src/home/FavouriteArticles.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ArticleCards from '../components/ArticleCards';
+import styles from './FavouriteArticles.module.css'; // Import the CSS module
 
 const FavouriteArticles = () => {
     const [articles, setArticles] = useState([]);
 
-    useEffect( () => {
-        fetch("http://localhost:5000/all-articles").then(res => res.json()).then(data => setArticles(data.slice(0,8)))
-    }, [])
+    useEffect(() => {
+        fetch("http://localhost:5000/all-articles")
+            .then(res => res.json())
+            .then(data => setArticles(data.slice(0,8)))
+    }, []);
 
-  return (
-    <div>
-        <ArticleCards articles={articles} headline="Top Articles This Week"/> 
-    </div>
-  )
+    return (
+        <div className={styles.favouriteArticlesContainer}> 
+            <ArticleCards articles={articles} headline="Top Articles This Week"/> 
+        </div>
+    )
 }
 
-export default FavouriteArticles
+export default FavouriteArticles;
